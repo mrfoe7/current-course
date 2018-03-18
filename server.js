5b7b13709b3a932e0c2a8136c6a6b25a0591e168
Semen Shaplygin <semyuon@gmail.com>
2018-03-18 21:49:40 +0300

    pre finnaly version of server

--- server.js
+++ server.js
@@ -1,60 +1,41 @@
 const http = require('http');
-const https = require('https');
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
 
+const routers = require('./routers');
 const config = require('./config');
-// const conn = require('./controllers');
-// const order = require('./routers');
+const ejsLayouts = require("express-ejs-layouts");
+// const conntrollers = require('./controllers');
 
 const app = express();
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-// require('./routes')(app);
-// app.use('/order', order);
-let url = 'http://www.cbr.ru/scripts/XML_daily.asp';
-// let currDate = new Date();
-// currDate.toISOString().substring(0, 10);
-//<CharCode>AUD</CharCode> <Value>44,7937</Value> http://www.cbr.ru/scripts/XML_daily.asp
-// let query = `GetCursOnDate({'On_date':${currDate}})`;
 
 app.set('port', config.get('port') || 8080)
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
-function getOrder(){
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(ejsLayouts);
+app.use('/', routers);
 
-}
-function getLocator(){
-
-}
-app.get('/orders', (req, res)=>{
-  console.log(config.get('mysql'));
-  console.log('orders');
-})
-
-app.get('/orders/:field', (req, res)=>{
-  console.log('orders/field');
-})
-
-app.use((req, res, next)=>{
-  let err = new Error('pages not found');
-  err.status = 404;
-  next(err);
-})
-
-app.use((err, req, res, next)=>{
-  res.status(err.status || 500);
-  res.render('error', {
-    message: err.message,
-    error: app.get( ('env') === 'dev') ? err: {}
-  });
-})
+// app.use((req, res, next)=>{
+//   let err = new Error('pages not found');
+//   err.status = 404;
+//   next(err);
+// })
+//
+// app.use((err, req, res, next)=>{
+//   res.status(err.status || 500);
+//   res.render('error', {
+//     message: err.message,
+//     error: app.get( ('env') === 'dev') ? err: {}
+//   });
+// })
 
 const server = http.createServer(app);
-server.listen(config.get('port'), ()=>{
+server.listen(config.get('port') || 8080, ()=>{
   let port = config.get("port") || 8080;
   console.log('Server runnig on port - ' + port);
 })
