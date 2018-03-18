1945ed14839160c1c04b2efe399d4bf71702529b
Semen Shaplygin <semyuon@gmail.com>
2018-03-18 21:50:04 +0300

    index routers

--- routers/index.js
+++ routers/index.js
@@ -1,12 +1,29 @@
 const express = require('express');
-let router = express.Router();
 
-// router.get('/' ()=>{
-//
-// })
+let routers = express.Router();
+
+routers.route('/')
+    .get(require('./main').get);
+
+routers.route('/orders')
+    .get(require('./orders').get);
+
+routers.route('/orders/locators')
+    .get(require('./locators').get);
+
+    
+// routers.route('/loc')
+//     .get( (req, res)=>{
+//       res.send('orders/Локатор');
+//   // res.render("orders", {
+//   //      title: "Таблица пассажиров",
+//   //      passengers: ["gavgav@mycorp.com", "mioaw@mycorp.com"]
+//   //  });
+//   console.log('orders/Локатор');
+// });
 //
 // router.get('' ()=>{
 //
 // })
 
-modele.exports = router;
+module.exports = routers;
