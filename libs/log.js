0e405194a18b6a991eb23536efd6500e945c9889
Semen Shaplygin <semyuon@gmail.com>
2018-03-21 22:09:44 +0300

    setup logs

--- libs/log.js
+++ libs/log.js
@@ -1,17 +1,45 @@
-var winston = require('winston');
+const winston = require('winston');
+const config = require('../config');
+
 var ENV = process.env.NODE_ENV;
 
 function getLogger(module) {
 
   var path = module.filename.split('/').slice(-2).join('/');
-
-  return new winston.Logger({
+  // winston.add(winston.transports.File, { filename: 'somefile.log' });
+  return new winston.createLogger({
     transports: [
       new winston.transports.Console({
+        name: 'cosnsole',
         colorize: true,
         level: (ENV == 'development') ? 'debug' : 'error',
         label: path
+        // timestamp: () => {
+        //   return Date.now();
+        // },
       })
+      // new winston.transports.File({
+      //   name: 'info-file',
+      //   filename: 'filelog-info.log',
+      //   colorize: true,
+      //   level: 'log',
+      //   label: path,
+      //   timestamp: () => {
+      //     let date = new Date();
+      //     return date.getUTCString();
+      //   },
+      // }),
+      // new winston.transports.File({
+      //   name: 'error-file',
+      //   filename: 'filelog-error.log',
+      //   colorize: true,
+      //   level: 'error',
+      //   label: path,
+      //   timestamp: () => {
+      //     let date = new Date();
+      //     return date.getUTCString();
+      //   },
+      // })
     ]
   });
 }
