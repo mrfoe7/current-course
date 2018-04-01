2908c28a3f38dc68de167d103149196eea859e5a
Semen Shaplygin <semyuon@gmail.com>
2018-04-01 13:07:43 +0300

    add SOAP client

--- app-service/app/controllers/orders.js
+++ app-service/app/controllers/orders.js
@@ -2,6 +2,27 @@ const https = require('https');
 const config = require('../config');
 const db = require('../libs/db');
 const soap = require('soap');
+const {promisify} = require('util');
+const logger = require('../libs/log')(module);
+const memcached = require('memcached');
+
+
+function currentFDate(){
+  let date = new Date();
+  let month = '',
+      day = '';
+  if (date.getMonth() + 1 < 10){
+    month = `0${date.getMonth() + 1}`;
+  }else{
+    month = `${date.getMonth() + 1}`;
+  }
+  if (date.getDate() < 10){
+    day = `0${date.getDate()}`;
+  }else{
+    day = `${date.getDate()}`;
+  }
+  return `${date.getFullYear()}-${month}-${day}`;
+}
 
 function getCourseJ(url){
   return new Promise( (resolve, reject) => {
@@ -11,7 +32,13 @@ function getCourseJ(url){
         chunks.push(data);
       }).on('end', () =>{
           let data = Buffer.concat(chunks);
-          resolve(JSON.parse(data));
+          data = JSON.parse(data);
+
+          delete data.PreviousDate;
+          delete data.Timestamp;
+          delete data.PreviousURL;
+
+          resolve(data);
       });
     })
       .on('error', (err)=>{
@@ -20,81 +47,89 @@ function getCourseJ(url){
   });
 }
 
+function getCourseX(url){
 
+  let currentDate = currentFDate();
+  let args = {"On_date" : currentDate};
+  return new Promise((resolve, reject)=>{
+    soap.createClient(url, (err, client)=>{
+      if (err) reject(err);
+      console.log(err);
+      client.GetCursOnDate(args, (err, result)=>{
+        if (err) reject(err);
+        let chResult = {};
+        chResult['Date'] = new Date();
+        chResult['Valute'] = {};
+        result = Object.assign([], result.GetCursOnDateResult.diffgram.ValuteData.ValuteCursOnDate);
+
+        result.forEach ( (item, index)=>{
+          item['NumCode'] = item.Vcode;
+          item['Name'] = item.Vname;
+          item['Value'] = item.Vcurs;
+          item['CharCode'] = item.VchCode;
+          item['Nominal'] = item.Vnom;
+          delete item.attributes;
+          delete item.Vcode;
+          delete item.Vname;
+          delete item.Vcurs;
+          delete item.VchCode;
+          delete item.Vnom;
+          chResult.Valute[item.CharCode] = item;
+        });
+        resolve(chResult);
+      });
+    });
+  });
+}
 
 async function getOrders(){
   try {
-   //let urlj = 'https://www.cbr-xml-daily.ru/daily_json.js';
-   let url = 'http://www.cbr.ru/DailyInfoWebServ/DailyInfo.asmx?WSDL';
-   let currentDate = new Date();
-   let args = {'On_date':`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`};
-   console.log(args);
+   let urlJ = 'https://www.cbr-xml-daily.ru/daily_json.js';
+   let urlX = 'http://www.cbr.ru/DailyInfoWebServ/DailyInfo.asmx?WSDL';
 
-   soap.createClientAsync(url).then((client)=>{
-     return client.GetCursOnDate(args);
-   }).then((result)=>{
-     console.log(result);
-   });
-   //GetCursOnDate({'On_date':`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`})
-   //let res = await getCourse(url);
-   // let query = 'SELECT o.id, o.locator, o.date_insert, o.price, o.currency, t.amount FROM `orders` o left join (select distinct order_id, count(order_id) as amount from `order_passengers` group by order_id) t on o.id = t.order_id;';
-   // let conn = await db.connectDB(config.get('mysql'));
-   // let rows = await db.getData(conn, query);
-   // await db.closeDB(conn);
-   // let mass = [];
-   //
-   // for (let index in rows){
-   //   let value_rus = '-';
-   //   let value_сurrency = '-';
-   //   let name_currency = rows[index].currency.toUpperCase();
-   //   if (name_currency !== 'RUB' ){
-   //     value_rus = rows[index].price * (res.Valute[name_currency].Value/res.Valute[name_currency].Nominal);
-   //     value_сurrency = `${rows[index].price} ${rows[index].currency}`
-   //   }else{
-   //     value_rus = rows[index].price;
-   //     value_сurrency = '-';
-   //   }
-   //   rows[index].price_rus = value_rus;
-   //   rows[index].price_сurrency = value_сurrency;
-   //   mass.push(rows[index]);
-   // }
-   // return mass;
+   let res = null;
+   let resX = await getCourseX(urlX);
+   let resJ = await getCourseJ(urlJ);
+   res = (resX == null) ? resJ : resX;
+   console.log(res);
+
+   let query = 'SELECT o.id, o.locator, o.date_insert, o.price, o.currency, t.amount FROM `orders` o left join (select distinct order_id, count(order_id) as amount from `order_passengers` group by order_id) t on o.id = t.order_id;';
+   let conn = await db.connectDB(config.get('mysql'));
+   let rows = await db.getData(conn, query);
+   await db.closeDB(conn);
+   let mass = [];
+
+   for (let index in rows){
+     let value_rus = '-';
+     let value_сurrency = '-';
+     let name_currency = rows[index].currency.toUpperCase();
+     if (name_currency != 'RUB' ){
+       value_rus = rows[index].price * (res.Valute[name_currency].Value/res.Valute[name_currency].Nominal);
+       value_rus = value_rus.toFixed(4);
+       value_сurrency = `${rows[index].price} ${rows[index].currency}`
+     }else{
+       value_rus = rows[index].price;
+       value_сurrency = '-';
+     }
+     rows[index].price_rus = value_rus;
+     rows[index].price_сurrency = value_сurrency;
+     mass.push(rows[index]);
+   }
+   return mass;
  } catch (err) {
-  throw err;
+    logger.error(err);
+    throw err;
  }
 }
 
-
-
 module.exports = (req, res)=>{
-  let url = 'http://www.cbr.ru/DailyInfoWebServ/DailyInfo.asmx?wsdl';
-  let currentDate = new Date();
-  // let args = {'On_date':`${currentDate.getFullYear()}-${currentDate.getMonth() + 1}-${currentDate.getDate()}`};
-  // console.log(args);
-  let args = {'On_date': '2018-03-21'}
-  console.log(args);
-  // soap.createClientAsync(url).then((client)=>{
-  //   return client.GetCursOnDate(args);
-  // }).then( (result)=>{
-  //   console.log(result);
-  // });
-  let ress = null;
-  soap.createClient(url, function(err, client) {
-    client.GetCursOnDate(args, function(err, result) {
-        console.log(result);
-        ress = Object.assign({}, result);
-        // console.log(result.GetCursOnDateResult, result.GetCursOnDateResult.schema, result.GetCursOnDateResult.diffgram);
-    });
-});
-
-  let mass = [];
-  // let promise = getOrders();
-  // promise.then( (result)=>{
-  //   let rows = Object.assign({}, result);
-  //   let mass = [];
-  //   for (let index in rows){
-  //       mass.push(rows[index]);
-  //   }
-    res.render('orders', { title: "Таблица заказов", rows:mass, ress:ress});
-  // });
+  getOrders().then( (result)=>{
+    let mass = [];
+    for (let index in result){
+        mass.push(result[index]);
+    }
+    // console.log(mass);
+    logger.info(`Sending data to /orders to user`);
+    res.render('orders', { title: "Таблица заказов", rows:mass});
+  });
 };
