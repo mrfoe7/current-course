29b28e686ede1003916064f04fe7596ffac4eb4d
Semen Shaplygin <semyuon@gmail.com>
2018-04-01 14:09:52 +0300

    update cached

--- app-service/app/libs/cached.js
+++ app-service/app/libs/cached.js
@@ -1,5 +1,6 @@
 const memcached = require('memcached');
 const config = require('../config');
 
+let client  = new Memcached(config.get('memcached:host'), config.get('memached:port'));
 
 module.exports = {};
